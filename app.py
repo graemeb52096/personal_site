82012115196739260d62e7d4b696aefe82b5621f
Graeme Bates <BatesG1996@wifihost-31-101.wireless.utsc.utoronto.ca>
2014-10-01 00:33:48 -0400

    Added Templates and static files

---
+++ app.py
@@ -0,0 +1,42 @@
+import web
+
+urls = (
+    '/', 'index',
+    '/blog', 'blog',
+    '/about', 'about',
+    '/photography', 'photography',
+    '/resume', 'resume',
+    '/static/images/photos/.*' 'images'
+)
+
+render = web.template.render('templates/', base='base')
+
+class index:
+    def GET(self):
+        return render.index()        
+class blog:
+    def GET(self):
+        return render.blog()
+class about:
+    def GET(self):
+        return render.about()
+class photography:
+    def GET(self):
+        import os
+
+        total_con=os.listdir('static/images/photos')
+
+        files=[]
+
+        for f_n in total_con:
+            if f_n.split('.')[1] == 'jpg':
+                files.append(f_n)
+        
+        return render.photography(files)
+class resume:
+    def GET(self):
+        return render.resume()
+        
+if __name__ == "__main__": 
+    app = web.application(urls, globals())
+    app.run() 
